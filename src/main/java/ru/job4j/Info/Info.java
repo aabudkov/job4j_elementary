de8654182afcd0af96882472136b45805cad858f
aabudkov <budkovaa1@gmail.com>
2022-11-29 22:49:10 +0300

    Проставил пробелы перед {

--- src/main/java/ru/job4j/Info/Info.java
+++ src/main/java/ru/job4j/Info/Info.java
@@ -1,7 +1,7 @@
-package ru.job4j.Info;
+package ru.job4j.info;
 
 public class Info {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("29.11.2022");
     }
 }
